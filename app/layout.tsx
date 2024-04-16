3c569c5da94c2fd266d3fd8b7e0b21247e9dfdb2
cheri <huanyue.zx@raycloud.com>
2024-04-16 20:49:37 +0800

    feat: first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,7 @@
+import '@/app/ui/global.css'
+import { inter } from '@/app/ui/fonts';
+import SideNav from '@/app/ui/dashboard/sidenav';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +9,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+        <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+            <div className="w-full flex-none md:w-64">
+            <SideNav />
+        </div>
+        <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+        </div>
     </html>
   );
 }
